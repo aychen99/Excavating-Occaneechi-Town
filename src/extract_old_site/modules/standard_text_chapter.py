15828e9a1450ea6a3bafd06e426d30cfffe10cfe
aychen99 <42681629+aychen99@users.noreply.github.com>
2020-09-26 16:25:34 -0400

    Add function and test to extract text page sidebar
    Also slightly reorganize the "tests" directory structure.

--- src/extract_old_site/modules/standard_text_chapter.py
+++ src/extract_old_site/modules/standard_text_chapter.py
@@ -1,17 +1,83 @@
 from bs4 import BeautifulSoup
+import pathlib
 
 def extract_page_content():
+    """Extract contents of a page from a report*b.html file."""
     pass
 
 def extract_page_title(html_string):
+    """Extract the page title from a report*a.html file."""
     soup = BeautifulSoup(html_string, 'html.parser')
     return str(soup.body.center.i.string)
 
-def extract_page_number():
-    pass
+def extract_page_number(html_string):
+    """Extract the page number from a report*c.html file."""
+    soup = BeautifulSoup(html_string, 'html.parser')
+    # Note: Leaves page number as a string due to Roman numerals
+    return str(soup.body.center.string).replace('Page ', '')
 
-def extract_sidebar():
-    pass
+def extract_sidebar(html_string, folder_path, parent_body_page_name):
+    """Extract sidebar info from a "index*_*.html" file."""
+    # Note: because the original html content did not have any closing </p>
+    # tags, this function depends on using html5lib for proper parsing.
+    soup = BeautifulSoup(html_string, 'html5lib')
+    paragraphs = soup.body.find_all('p')
+
+    currentModuleFullName = str(paragraphs[0].b.string).strip()
+    moduleAuthor = None
+    sections = None
+    p_tag_with_sections = -1
+    if len(paragraphs) == 1:
+        # Only the full module name is in this sidebar, no links or sections.
+        pass
+    elif len(paragraphs) == 2:
+        # Check to see if it's a combination of the module title & author(s),
+        # or of the module title and section links.
+        if len(paragraphs[1].find_all('a')) == 0:
+            moduleAuthor = str(paragraphs[1].string).strip()
+        else:
+            p_tag_with_sections = 1
+    else:
+        moduleAuthor = str(paragraphs[1].string).strip()
+        p_tag_with_sections = 2
+
+    if p_tag_with_sections > 0:
+        sections = []
+        links_contents = paragraphs[p_tag_with_sections].contents
+        i = 0
+        while i < len(links_contents) - 1:
+            section_object = None
+            content = links_contents[i]
+            if content.name == 'a':
+                section_object = {
+                    'name': str(content.string).strip(),
+                    'path': str(pathlib.PurePosixPath(folder_path) / str(content['href'])),
+                    'subsections': []
+                }
+            elif isinstance(content, str) and content.strip() != '':
+                # Then it's the link to the current page, without an <a> tag around it.
+                section_object = {
+                    'name': str(content.string).strip(),
+                    'path': str(pathlib.PurePosixPath(folder_path) / parent_body_page_name),
+                    'subsections': []
+                }
+
+            if section_object:
+                if i != 0 and ('\xa0' in links_contents[i-1] or '\xa0' in content):
+                    # Covers both the case where there's an <a> tag around the
+                    # title, and when it's the current page so there's no <a> tag.
+                    # Relies on fact that no sidebar has more than three levels of
+                    # links, i.e. subsections do not themselves have subsections.
+                    sections[-1]['subsections'].append(section_object)
+                else:
+                    sections.append(section_object)
+            i += 1
+
+    return {
+        'currentModuleFullName': currentModuleFullName,
+        'moduleAuthor': moduleAuthor,
+        'sections': sections
+    }
 
 def extract_topbar():
     pass
---
+++ tests/modules/test_standard_text_chapter.py
@@ -0,0 +1,169 @@
+from src.extract_old_site.modules import standard_text_chapter
+
+def test_extract_page_title():
+    test_data = "<html><body><center><i>Historical Background</i></center></body></html>"
+    assert standard_text_chapter.extract_page_title(test_data) == "Historical Background"
+
+def test_extract_page_number():
+    test_data1 = "<html><body><center>Page i</center></body></html>"
+    test_data2 = "<html><body><center>Page 51</center></body></html>"
+    assert standard_text_chapter.extract_page_number(test_data1) == "i"
+    assert standard_text_chapter.extract_page_number(test_data2) == "51"
+
+def test_extract_sidebar():
+    test_data1 = """<html><body><p>
+                    <b>Archaeological Background</b><br><p>
+                    by Dickens, Roy S., Jr., H. Trawick Ward, and R. P. Stephen Davis, Jr.<p>
+                    <a target="main" href="body0_1.html">Historical Background</a><br>
+                    Siouan Archaeology<br>
+                    <a target="main" href="body0_3.html">Fredricks Site Discovery</a><br>
+                    <a target="main" href="body0_4.html">Fredricks Site Excavation</a><br>
+                    <a target="main" href="body0_5.html">List of Figures</a><br>
+                    <a target="main" href="body0_6.html">Sources</a><br>
+                    </body></html>"""
+    test1_sections = [{
+        'name': 'Historical Background',
+        'path': '/dig/html/part2/body0_1.html',
+        'subsections': []
+    }, {
+        'name': 'Siouan Archaeology',
+        'path': '/dig/html/part2/body0_2.html',
+        'subsections': []
+    }, {
+        'name': 'Fredricks Site Discovery',
+        'path': '/dig/html/part2/body0_3.html',
+        'subsections': []
+    }, {
+        'name': 'Fredricks Site Excavation',
+        'path': '/dig/html/part2/body0_4.html',
+        'subsections': []
+    }, {
+        'name': 'List of Figures',
+        'path': '/dig/html/part2/body0_5.html',
+        'subsections': []
+    }, {
+        'name': 'Sources',
+        'path': '/dig/html/part2/body0_6.html',
+        'subsections': []
+    }]
+    extracted = standard_text_chapter.extract_sidebar(test_data1, '/dig/html/part2', 'body0_2.html')
+    assert extracted['currentModuleFullName'] == 'Archaeological Background'
+    assert extracted['moduleAuthor'] == 'by Dickens, Roy S., Jr., H. Trawick Ward, and R. P. Stephen Davis, Jr.'
+    assert extracted['sections'] == test1_sections
+
+    test_data2 = """<html><body><p>
+                    <b>Animal Remains: 1983-1984 Excavations</b><br><p>
+                    by Mary Ann Holm<p>
+                    <a target="main" href="body0_01.html">Research Questions</a><br>
+                    <a target="main" href="body0_02.html">Ethnohistoric Accounts of Animal Use</a><br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;<a target="main" href="body0_03.html">Mammals</a><br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;<a target="main" href="body0_04.html">Birds</a><br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;Reptiles<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;<a target="main" href="body0_06.html">Amphibians</a><br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;<a target="main" href="body0_07.html">Fish</a><br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;<a target="main" href="body0_08.html">Summary</a><br>
+                    <a target="main" href="body0_09.html">Recovery Techniques</a><br>
+                    <a target="main" href="body0_10.html">Analytical Procedures</a><br>
+                    <a target="main" href="body0_11.html">Animals from the Wall Site</a><br>
+                    <a target="main" href="body0_12.html">Animals from the Fredricks Site</a><br>
+                    <a target="main" href="body0_13.html">Comparison of the Sites</a><br>
+                    <a target="main" href="body0_14.html">Habitat Preference and Seasonality</a><br>
+                    <a target="main" href="body0_15.html">Species Diversity</a><br>
+                    <a target="main" href="body0_16.html">Conclusions</a><br>
+                    <a target="main" href="body0_17.html">List of Tables and Figures</a><br>
+                    <a target="main" href="body0_18.html">Source</a><br>
+                    </body></html>"""
+    test2_sections = [{
+        'name': 'Research Questions',
+        'path': '/dig/html/part4/body0_01.html',
+        'subsections': []
+    }, {
+        'name': 'Ethnohistoric Accounts of Animal Use',
+        'path': '/dig/html/part4/body0_02.html',
+        'subsections': [{
+            'name': 'Mammals',
+            'path': '/dig/html/part4/body0_03.html',
+            'subsections': []
+        }, {
+            'name': 'Birds',
+            'path': '/dig/html/part4/body0_04.html',
+            'subsections': []
+        }, {
+            'name': 'Reptiles',
+            'path': '/dig/html/part4/body0_05.html',
+            'subsections': []
+        }, {
+            'name': 'Amphibians',
+            'path': '/dig/html/part4/body0_06.html',
+            'subsections': []
+        }, {
+            'name': 'Fish',
+            'path': '/dig/html/part4/body0_07.html',
+            'subsections': []
+        }, {
+            'name': 'Summary',
+            'path': '/dig/html/part4/body0_08.html',
+            'subsections': []
+        }]
+    }, {
+        'name': 'Recovery Techniques',
+        'path': '/dig/html/part4/body0_09.html',
+        'subsections': []
+    }, {
+        'name': 'Analytical Procedures',
+        'path': '/dig/html/part4/body0_10.html',
+        'subsections': []
+    }, {
+        'name': 'Animals from the Wall Site',
+        'path': '/dig/html/part4/body0_11.html',
+        'subsections': []
+    }, {
+        'name': 'Animals from the Fredricks Site',
+        'path': '/dig/html/part4/body0_12.html',
+        'subsections': []
+    }, {
+        'name': 'Comparison of the Sites',
+        'path': '/dig/html/part4/body0_13.html',
+        'subsections': []
+    }, {
+        'name': 'Habitat Preference and Seasonality',
+        'path': '/dig/html/part4/body0_14.html',
+        'subsections': []
+    }, {
+        'name': 'Species Diversity',
+        'path': '/dig/html/part4/body0_15.html',
+        'subsections': []
+    }, {
+        'name': 'Conclusions',
+        'path': '/dig/html/part4/body0_16.html',
+        'subsections': []
+    }, {
+        'name': 'List of Tables and Figures',
+        'path': '/dig/html/part4/body0_17.html',
+        'subsections': []
+    }, {
+        'name': 'Source',
+        'path': '/dig/html/part4/body0_18.html',
+        'subsections': []
+    }]
+    extracted = standard_text_chapter.extract_sidebar(test_data2, '/dig/html/part4', 'body0_05.html')
+    assert extracted['currentModuleFullName'] == 'Animal Remains: 1983-1984 Excavations'
+    assert extracted['moduleAuthor'] == 'by Mary Ann Holm'
+    assert extracted['sections'] == test2_sections
+
+
+def test_extract_frames():
+    def readfile(filename):
+        if filename == 'report33a.html':
+            return "a"
+        elif filename == 'report33b.html':
+            return "b"
+        elif filename == 'report33c.html':
+            return "c"
+    
+    test_data = """ <html><frameset rows="28,*,28" border=1>
+                    <frame scrolling="no" src="report33a.html" marginwidth=1 marginheight=1>
+                    <frame src="report33b.html" marginwidth=1 marginheight=1>
+                    <frame scrolling="no" src="report33c.html" marginwidth=1 marginheight=1>
+                    </frameset><noframes>you need frames</noframes></html>"""
+    assert standard_text_chapter.extract_frames(test_data, readfile) == ["a", "b", "c"]
