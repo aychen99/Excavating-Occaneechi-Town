5440649806b1d1084f01e0e4b3297462970ac7d6
Jacob King <jmking628@gmail.com>
2020-11-04 21:06:30 -0500

    Fix encoding issue for table extraction on OSX

--- src/extract_old_site/utilities/file_ops.py
+++ src/extract_old_site/utilities/file_ops.py
@@ -2,7 +2,6 @@ import pathlib
 
 def readfile(filename, current_dir_path):
     """Return the contents of a file as a string given a Path object to it.
-    
     Parameters
     ----------
     filename : str
@@ -11,5 +10,5 @@ def readfile(filename, current_dir_path):
         pathlib.Path object for where the file is found on the system.
     """
     file_path = current_dir_path / filename
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r', encoding='ISO-8859-1') as f:
         return f.read()
