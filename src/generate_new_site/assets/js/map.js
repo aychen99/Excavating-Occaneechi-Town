8c45499dfc56f1fb1bc6b0e1582864cce8f4d656
aychen99 <42681629+aychen99@users.noreply.github.com>
2021-01-08 13:21:26 -0500

    Add favicons and Excavations map tooltip
    - Add favicons generated from realfavicongenerator.net. Note that the
    browserconfig.xml and site.webmanifest files have had their contents
    changed to use a relative link rather than an absolute path.
    - Add functionality for displaying a tooltip with the name of whichever
    context the user's mouse is hovering over when visiting the map in
    the "Excavations" chapter on desktop. Whether the map will disable the
    tooltip on mobile, as it is intended to, still needs to be tested.
    - Corrected spelling of "Occaneechi" in index.html.jinja.

--- src/generate_new_site/assets/js/map.js
+++ src/generate_new_site/assets/js/map.js
@@ -132,7 +132,7 @@ function runmap() {
         d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
       );
       context_href.attr("href", "#");
-      context_name.text("None");
+      context_name.text("None").style("color", "");
     }
 
     function clicked(event, d) {
@@ -147,13 +147,110 @@ function runmap() {
         d3.pointer(event, svg.node())
       );
       context_href.attr("href", hrefs[d.id]["href"]);
-      context_name.text(hrefs[d.id]["name"]);
+
+      // Change text and font color of text after "Selected Context:"
+      let name = hrefs[d.id]["name"];
+      let lowercaseName = name.toLowerCase();
+      let textColor = "";
+      if (lowercaseName.includes("feature") || lowercaseName.includes("burial")) {
+        textColor = "crimson";
+      } else if (lowercaseName.includes("structure")) {
+        textColor = "#FF4500";
+      } else if (lowercaseName.includes("sq")) {
+        textColor = "blue";
+      } else {
+        // Reset to default color when clicking on "None"
+      }
+      context_name.text(name).style("color", textColor);
     }
 
     function zoomed(event) {
       const {transform} = event;
       g.attr("transform", transform);
       g.attr("stroke-width", 1 / transform.k);
+    }
+
+    // Set up tooltip on hovering over a context
+    var tooltipDiv = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .attr("id", "d3tooltip")
+      .style("position", "absolute")
+      .style("display", "none")
+      .style("opacity", 0.93)
+      .style("background-color", "white")
+      .style("font-size", "1.2em")
+      .style("border", "2px solid gray")
+      .style("padding", "2px");
+
+    /* Handler functions for the tooltip when hovering over the map */
+    let mouseoverHandler = function(e) {
+      // Prevent tooltip from displaying on touchscreens
+      if (matchMedia("(hover: none) and (pointer: coarse)").matches) {
+        return;
+      }
+      let name = hrefs[e.target.id]["name"];
+      let lowercaseName = name.toLowerCase();
+      let textColor = "";
+      if (lowercaseName.includes("feature") || lowercaseName.includes("burial")) {
+        textColor = "crimson";
+      } else if (lowercaseName.includes("structure")) {
+        textColor = "#FF4500";
+      } else if (lowercaseName.includes("sq")) {
+        textColor = "blue";
+      } else {
+        // Do nothing
+      }
+      d3.select("#d3tooltip")
+        .style("display", "")
+        .style("color", textColor)
+        .text(hrefs[e.target.id]["name"]);
+    };
+
+    let mouseoutHandler = function(e) {
+      d3.select("#d3tooltip").style("display", "none");
+    };
+
+    let mousemoveHandler = function(e) {
+      if (matchMedia("(min-width: 768px").matches) {
+        d3.select("#d3tooltip")
+          .style("left", (e.pageX + 10) + "px")
+          .style("right", "")
+          .style("top", (e.pageY + 10) + "px");
+      } else {
+        // Determine whether the tooltip should be to the left or right of the
+        // cursor based on location in the page, i.e. Make tooltip direction
+        // change depending on where in the map it is, to avoid scrollbars from
+        // appearing on mobile or narrower screens
+        const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+        const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+        if (e.pageX > vw * 2 / 3) {
+          d3.select("#d3tooltip")
+            .style("left", "")
+            .style("right", (vw - (e.pageX) - 10) + "px")
+            .style("top", (e.pageY + 10) + "px");
+        } else {
+          d3.select("#d3tooltip")
+            .style("left", (e.pageX + 10) + "px")
+            .style("right", "")
+            .style("top", (e.pageY + 10) + "px");
+        }
+      }
+    };
+
+    /* 
+     * Adjust clickable areas and tooltip display depending on the pressed
+     * context button
+     */
+    function addTooltipHandlers(objectToAddTo) {
+      objectToAddTo.on("mouseover", mouseoverHandler)
+        .on("mouseout", mouseoutHandler)
+        .on("mousemove", mousemoveHandler);
+    }
+
+    function removeTooltipHandlers(objectToRemoveFrom) {
+      objectToRemoveFrom.on("mouseover", null)
+        .on("mouseout", null)
+        .on("mousemove", null);
     }
 
     function focusNone() {
@@ -164,6 +261,12 @@ function runmap() {
       g.select("#structures-internal").raise();
       g.select("#structures-external").raise();
       g.select("#features").raise();
+      removeTooltipHandlers(structures_external);
+      removeTooltipHandlers(features);
+      removeTooltipHandlers(grid);
+      addTooltipHandlers(structures_external);
+      addTooltipHandlers(features);
+      addTooltipHandlers(grid);
     }
 
     function focusGrid() {
@@ -174,6 +277,10 @@ function runmap() {
       g.select("#structures-external").raise();
       g.select("#features").raise();
       g.select("#grid").raise();
+      removeTooltipHandlers(d3.select("svg").selectAll("path"));
+      removeTooltipHandlers(structures_external);
+      removeTooltipHandlers(features);
+      addTooltipHandlers(grid);
     }
 
     function focusFeatures() {
@@ -184,6 +291,10 @@ function runmap() {
       g.select("#structures-internal").raise();
       g.select("#structures-external").raise();
       g.select("#features").raise();
+      removeTooltipHandlers(d3.select("svg").selectAll("path"));
+      removeTooltipHandlers(structures_external);
+      removeTooltipHandlers(grid);
+      addTooltipHandlers(features);
     }
 
     function focusStructures() {
@@ -194,7 +305,14 @@ function runmap() {
       g.select("#grid").raise();
       g.select("#structures-internal").raise();
       g.select("#structures-external").raise();
+      removeTooltipHandlers(d3.select("svg").selectAll("path"));
+      removeTooltipHandlers(features);
+      removeTooltipHandlers(grid);
+      addTooltipHandlers(structures_external);
     }
+
+    // Run focusNone to start
+    focusNone();
 
     return;
 }
